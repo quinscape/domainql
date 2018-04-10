f4d570b8525cfbb7219e22c3a31d69451b5f310a
Sven Helmberger <sven.helmberger@quinscape.de>
2018-04-10 22:43:42 +0200

    Refactor RelationConfiguration
    Don't work off the foreignKey constants of the Keys class but instead
    directly work with the generated table types. For example
    
        FOO.TARGET_ID
    
    Resolve the fields in the builder to the foreign key referencing them or
    throw an error if that's not possible.

--- src/main/java/de/quinscape/domainql/DomainQLBuilderException.java
+++ src/main/java/de/quinscape/domainql/DomainQLBuilderException.java
@@ -1,5 +1,33 @@
 package de.quinscape.domainql;
 
 public class DomainQLBuilderException
+    extends RuntimeException
 {
+    private static final long serialVersionUID = -6107054351550261974L;
+
+
+    public DomainQLBuilderException(String message)
+    {
+        super(message);
+    }
+
+
+    public DomainQLBuilderException(String message, Throwable cause)
+    {
+        super(message, cause);
+    }
+
+
+    public DomainQLBuilderException(Throwable cause)
+    {
+        super(cause);
+    }
+
+
+    public DomainQLBuilderException(
+        String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace
+    )
+    {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
