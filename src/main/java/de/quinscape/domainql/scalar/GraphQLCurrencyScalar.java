5265ad2e91dec286bf730d5e2537b44701b2b896
Sven Helmberger <sven.helmberger@quinscape.de>
2018-04-26 17:25:36 +0200

    fix currency

--- src/main/java/de/quinscape/domainql/scalar/GraphQLCurrencyScalar.java
+++ src/main/java/de/quinscape/domainql/scalar/GraphQLCurrencyScalar.java
@@ -1,11 +1,14 @@
 package de.quinscape.domainql.scalar;
 
+import graphql.language.IntValue;
 import graphql.language.StringValue;
 import graphql.schema.Coercing;
 import graphql.schema.CoercingParseLiteralException;
 import graphql.schema.CoercingParseValueException;
 import graphql.schema.CoercingSerializeException;
 import graphql.schema.GraphQLScalarType;
+
+import java.math.BigInteger;
 
 /**
  * GraphQL Scalar implementation for currency values. Stored as Long value in 1/10000th currency units. (e.g. 1.15 EUR = 11500 )
@@ -15,39 +18,39 @@ public class GraphQLCurrencyScalar
 {
     public GraphQLCurrencyScalar()
     {
-        super("Currency", "Currency value", new Coercing<Long, String>(){
+        super("Currency", "Currency value", new Coercing<Long, Long>(){
             @Override
-            public String serialize(Object dataFetcherResult) throws CoercingSerializeException
+            public Long serialize(Object dataFetcherResult) throws CoercingSerializeException
             {
                 if (dataFetcherResult == null)
                 {
                     return  null;
                 }
 
-                long value = ((Number)dataFetcherResult).longValue();
-                return String.valueOf(value);
+                return ((Number)dataFetcherResult).longValue();
             }
 
 
             @Override
             public Long parseValue(Object input) throws CoercingParseValueException
             {
-                if (!(input instanceof String))
+                if (!(input instanceof Number))
                 {
                     throw new CoercingParseValueException("Cannot coerce " + input + " to Currency");
                 }
 
-                return Long.parseLong((String) input);
+                return ((Number)input).longValue();
             }
 
             @Override
             public Long parseLiteral(Object input) throws CoercingParseLiteralException
             {
-                if (!(input instanceof StringValue))
+                if (!(input instanceof IntValue))
                 {
                     throw new CoercingParseValueException("Cannot coerce " + input + " to Currency");
                 }
-                return Long.parseLong(((StringValue) input).getValue());
+                final BigInteger value = ((IntValue) input).getValue();
+                return value.longValue();
             }
         });
     }
