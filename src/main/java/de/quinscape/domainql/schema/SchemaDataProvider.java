a45317740adc253578f33b44a7eed3b45537591a
Sven Helmberger <sven.helmberger@quinscape.de>
2019-04-26 15:12:09 +0200

    Draw out IntrospectionUtil
    So far we've only used GraphQL introspection inside the
    SchemaDataProvider, but now we also need it outside of that.
    
    We refactor the query itself and the basic execution of it out into an
    util class de.quinscape.domainql.util.IntrospectionUtil

--- src/main/java/de/quinscape/domainql/schema/SchemaDataProvider.java
+++ src/main/java/de/quinscape/domainql/schema/SchemaDataProvider.java
@@ -1,6 +1,7 @@
 package de.quinscape.domainql.schema;
 
 import de.quinscape.domainql.DomainQL;
+import de.quinscape.domainql.util.IntrospectionUtil;
 import de.quinscape.domainql.util.JSONHolder;
 import de.quinscape.spring.jsview.JsViewContext;
 import de.quinscape.spring.jsview.JsViewProvider;
@@ -26,75 +27,6 @@ public final class SchemaDataProvider
     private final JSONHolder schemaData;
 
     private final String viewDataName;
-
-    // language=GraphQL
-    private final static String INTROSPECTION_QUERY =
-        "query InputTypesQuery\n" +
-            "{\n" +
-            "    __schema {\n" +
-            "        types {\n" +
-            "            ...FullType\n" +
-            "        }\n" +
-            "    }\n" +
-            "}\n" +
-            "\n" +
-            "fragment FullType on __Type {\n" +
-            "    kind\n" +
-            "    name\n" +
-            "    description\n" +
-            "    fields(includeDeprecated: true) {\n" +
-            "        name\n" +
-            "        description\n" +
-            "        args {\n" +
-            "            ...InputValue\n" +
-            "        }\n" +
-            "        type {\n" +
-            "            ...TypeRef\n" +
-            "        }\n" +
-            "        isDeprecated\n" +
-            "        deprecationReason\n" +
-            "    }\n" +
-            "    inputFields {\n" +
-            "        ...InputValue\n" +
-            "    }\n" +
-            "    interfaces {\n" +
-            "        ...TypeRef\n" +
-            "    }\n" +
-            "    enumValues(includeDeprecated: true) {\n" +
-            "        name\n" +
-            "        description\n" +
-            "        isDeprecated\n" +
-            "        deprecationReason\n" +
-            "    }\n" +
-            "                possibleTypes {\n" +
-            "                    ...TypeRef\n" +
-            "                }\n" +
-            "            }\n" +
-            "\n" +
-            "            fragment InputValue on __InputValue {\n" +
-            "                name\n" +
-            "                description\n" +
-            "                type { ...TypeRef }\n" +
-            "                defaultValue\n" +
-            "            }\n" +
-            "\n" +
-            "            fragment TypeRef on __Type {\n" +
-            "                kind\n" +
-            "                name\n" +
-            "                ofType {\n" +
-            "                    kind\n" +
-            "                    name\n" +
-            "                    ofType {\n" +
-            "                        kind\n" +
-            "                        name\n" +
-            "                        ofType {\n" +
-            "                            kind\n" +
-            "                            name\n" +
-            "                        }\n" +
-            "                    }\n" +
-            "                }\n" +
-            "            }\n" +
-            "        ";
 
 
     /**
@@ -130,16 +62,8 @@ public final class SchemaDataProvider
     public SchemaDataProvider(DomainQL domainQL, String viewDataName, boolean appendGenericTypes)
     {
 
-        final GraphQL graphQL = GraphQL.newGraphQL(domainQL.getGraphQLSchema()).build();
-
-        ExecutionInput executionInput = ExecutionInput.newExecutionInput()
-            .query(INTROSPECTION_QUERY)
-            .build();
-
-        ExecutionResult executionResult = graphQL.execute(executionInput);
-        Map<String, Object> result = executionResult.getData();
-
-        final Map<String, Object> schemaRoot = (Map<String, Object>) result.get("__schema");
+        final Map<String, Object> schema = IntrospectionUtil.introspect(domainQL.getGraphQLSchema());
+        final Map<String, Object> schemaRoot = (Map<String, Object>) schema.get("__schema");
         if (appendGenericTypes)
         {
             schemaRoot.put(GENERIC_TYPES, domainQL.getGenericTypes());
@@ -148,6 +72,7 @@ public final class SchemaDataProvider
         this.schemaData = new JSONHolder(schemaRoot);
         this.viewDataName = viewDataName;
     }
+
 
 
     @Override
---
+++ src/main/java/de/quinscape/domainql/util/IntrospectionUtil.java
@@ -0,0 +1,110 @@
+package de.quinscape.domainql.util;
+
+import de.quinscape.domainql.DomainQL;
+import graphql.ExecutionInput;
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.schema.GraphQLSchema;
+
+import java.util.Map;
+
+/**
+ * GraphQL schema introspection utility class used by {@link de.quinscape.domainql.schema.SchemaDataProvider}.
+ */
+public final class IntrospectionUtil
+{
+    // language=GraphQL
+    public final static String INTROSPECTION_QUERY =
+        "query InputTypesQuery\n" +
+            "{\n" +
+            "    __schema {\n" +
+            "        types {\n" +
+            "            ...FullType\n" +
+            "        }\n" +
+            "    }\n" +
+            "}\n" +
+            "\n" +
+            "fragment FullType on __Type {\n" +
+            "    kind\n" +
+            "    name\n" +
+            "    description\n" +
+            "    fields(includeDeprecated: true) {\n" +
+            "        name\n" +
+            "        description\n" +
+            "        args {\n" +
+            "            ...InputValue\n" +
+            "        }\n" +
+            "        type {\n" +
+            "            ...TypeRef\n" +
+            "        }\n" +
+            "        isDeprecated\n" +
+            "        deprecationReason\n" +
+            "    }\n" +
+            "    inputFields {\n" +
+            "        ...InputValue\n" +
+            "    }\n" +
+            "    interfaces {\n" +
+            "        ...TypeRef\n" +
+            "    }\n" +
+            "    enumValues(includeDeprecated: true) {\n" +
+            "        name\n" +
+            "        description\n" +
+            "        isDeprecated\n" +
+            "        deprecationReason\n" +
+            "    }\n" +
+            "                possibleTypes {\n" +
+            "                    ...TypeRef\n" +
+            "                }\n" +
+            "            }\n" +
+            "\n" +
+            "            fragment InputValue on __InputValue {\n" +
+            "                name\n" +
+            "                description\n" +
+            "                type { ...TypeRef }\n" +
+            "                defaultValue\n" +
+            "            }\n" +
+            "\n" +
+            "            fragment TypeRef on __Type {\n" +
+            "                kind\n" +
+            "                name\n" +
+            "                ofType {\n" +
+            "                    kind\n" +
+            "                    name\n" +
+            "                    ofType {\n" +
+            "                        kind\n" +
+            "                        name\n" +
+            "                        ofType {\n" +
+            "                            kind\n" +
+            "                            name\n" +
+            "                        }\n" +
+            "                    }\n" +
+            "                }\n" +
+            "            }\n" +
+            "        ";
+
+    private IntrospectionUtil()
+    {
+        // no instances
+    }
+
+
+    /**
+     * Executes the {@link #INTROSPECTION_QUERY} against the given GraphQL schema.
+     *
+     * @param graphQLSchema     GraphQL schema
+     *
+     * @return raw introspection data including "__schema" key.
+     */
+    public static Map<String, Object> introspect(GraphQLSchema graphQLSchema)
+    {
+        final GraphQL graphQL = GraphQL.newGraphQL(graphQLSchema).build();
+
+        ExecutionInput executionInput = ExecutionInput.newExecutionInput()
+            .query(INTROSPECTION_QUERY)
+            .build();
+
+        ExecutionResult executionResult = graphQL.execute(executionInput);
+        return executionResult.getData();
+    }
+
+}
