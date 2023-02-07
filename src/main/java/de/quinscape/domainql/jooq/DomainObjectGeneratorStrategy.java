8c5dae3c32cafa47e62bae7739b7a92b83f802a1
Sven Helmberger <sven.helmberger@quinscape.de>
2023-02-07 17:44:25 +0100

    Upgrade JOOQ
    Upgrade JOOQ to the most current version and update the internal plugin
    dependency for postgres.

--- src/main/java/de/quinscape/domainql/jooq/DomainObjectGeneratorStrategy.java
+++ src/main/java/de/quinscape/domainql/jooq/DomainObjectGeneratorStrategy.java
@@ -1,7 +1,7 @@
 package de.quinscape.domainql.jooq;
 
-import org.jooq.util.DefaultGeneratorStrategy;
-import org.jooq.util.Definition;
+import org.jooq.codegen.DefaultGeneratorStrategy;
+import org.jooq.meta.Definition;
 
 /**
  * Makes all JOOQ POJOs extend the DomainObject interface.
