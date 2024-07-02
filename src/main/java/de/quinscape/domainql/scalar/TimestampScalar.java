545f57b966e9e1b3f2152e86a52915f07c1b4377
mdowideit <Marcel.Dowideit@quinscape.de>
2024-07-02 14:52:16 +0200

    Fixed wrong Timestamp-Conversion

--- src/main/java/de/quinscape/domainql/scalar/TimestampScalar.java
+++ src/main/java/de/quinscape/domainql/scalar/TimestampScalar.java
@@ -91,7 +91,7 @@ public class TimestampScalar
 
     public static String toISO8601(Timestamp dataFetcherResult)
     {
-        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
         return df.format(dataFetcherResult);
     }
 
